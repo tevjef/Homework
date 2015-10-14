f5b9f6130fb0ece03b8f6dbe8811e1f4c322ac7c
tevjef <mastermindtj94@gmail.com>
2015-10-14 02:17:30 -0400

    Project2 complete

--- CS-280/lex-project2/Tokenizer.cpp
+++ CS-280/lex-project2/Tokenizer.cpp
@@ -1,36 +1,99 @@
 #include "p2lex.h"
 #include <vector>
 
-Token getToken(istream *br, string& lexeme) {
-    char c;
-    string charbuf;
-    while (br-> good() && br->get(c)) {
-        charbuf+=c;
-        if (c == '{')  {
-            lexeme = c;
-            return LPAREN;
-        }
-        if (c == '}')  {
-            lexeme = c;
-            return RPAREN;
-        }
-        if (c == ';')  {
-            lexeme = c;
-            return SC;
-        }
-        if (c == '+')  {
-            lexeme = c;
-            return UNION;
-        }
-        if (c == '^')  {
-            lexeme = c;
-            return INTERSECT;
-        }
-        if (c == '\n')  {
-            linenum++;
-        }
-        if (isspace(c)) {
+int linenum = 1;
+int currentPosition = 0;
+string currentLine = "";
 
+void printError(string error);
+
+Token getToken(istream *br, string &lexeme) {
+    string charbuf;
+    bool inString = false;
+    bool inID = false;
+    bool inComment = false;
+    while (br->good()) {
+        char c;
+        br->get(c);
+        currentPosition++;
+        charbuf += c;
+        if (c != '\n') currentLine += c;
+        if (c == '\n') {
+            linenum++; //Increment line number
+            inComment = false; //Set flag to indicate that we're no longer in a comment.
+            if (inString) { //If we're in a string and hit a new line, exit.
+                printError(
+                        "Missing semicolon on " + to_string(linenum) + ":" + to_string(currentPosition) + "\n" + "\t" +
+                        currentLine);
+                return ERR;
+            }
+            currentLine = ""; //For logging, reset the current line
+            currentPosition = 0;  //For logging, reset the current position in line
         }
+        if (inComment) {
+            charbuf = "";
+            continue;
+        }
+        if (inString && isalpha(c)) {
+            continue;
+        }
+        if (c == '/') {
+            if (br->good() && br->peek() == '/') {
+                inComment = true;
+                continue;
+            }
+        }
+
+        if (isspace(c)) { // Space chars delimit identifiers, if we're in an ID return the ID. inString?
+            if (inID) {   // continue to the next letter since strings can have space
+                lexeme = charbuf;
+                return ID;
+            }
+            if (inString) {
+                continue;
+            }
+            charbuf = "";
+            continue;
+        }
+
+        if (c == '"' && !inString) {
+            inString = true;
+            continue;
+        }
+        if (c == '"' && inString) {
+            lexeme = charbuf;
+            return STRING;
+        }
+
+        lexeme = c;
+        if (c == '(') return LPAREN;
+        if (c == ')') return RPAREN;
+        if (c == ';') return SC;
+        if (c == '+') return UNION;
+        if (c == '^') return INTERSECT;
+
+        if (isalpha(c)) { // At this point, it's safe to say that the current char in the line is either a keyword or an
+            inID = true;  // identifier
+            lexeme = charbuf;
+            if (charbuf == "for") return FOR;
+            if (charbuf == "search")return SEARCH;
+            if (charbuf == "print") return PRINT;
+            if (charbuf == "set") return SET;
+            if (br->good() && !isalpha((char) br->peek())) return ID;
+            continue;
+        }
+        printError(
+                "Invalid identifier found on " + to_string(linenum) + ":" + to_string(currentPosition) + "\n" + "\t" +
+                currentLine);
+        return ERR;
     }
+    if (br->eof()) {
+        return DONE;
+    }
+}
+
+void printError(string error) {
+    cerr << error << endl << "\t";
+    for (int i = 0; i < currentPosition - 1; i++) cerr << " ";
+    cerr << "^" << endl;
 }
--- CS-280/lex-project2/lex.cpp
+++ CS-280/lex-project2/lex.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <set>
+#include <map>
+#include <vector>
+#include "p2lex.h"
 
 using namespace std;
 
@@ -9,15 +13,15 @@ void usage(char *progname, string msg) {
     cerr << " specifying filename reads from that file; no filename reads standard input" << endl;
 }
 
-int main(int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
     istream *br;
     ifstream infile;
 
-    if( argc == 1 )
+    if (argc == 1)
         br = &cin;
-    else if( argc == 2 ) {
+    else if (argc == 2) {
         infile.open(argv[1]);
-        if( infile.is_open() )
+        if (infile.is_open())
             br = &infile;
         else {
             usage(argv[0], "Cannot open " + string(argv[1]));
@@ -29,13 +33,63 @@ int main(int argc, char *argv[] ) {
         return 1;
     }
 
-    string oneLine;
+    string lexeme = "";
+    Token token;
+    map<string, int> identifiers;
+    map<Token, int> tokens;
+    multimap<int, Token> tokensByCount;
 
-    getline(*br, oneLine);
-    if( br->good() )
-        cout << oneLine << endl;
-    else
-        cout << "!!! I COULD NOT READ !!!\n";
+    while (token = getToken(br, lexeme), token != DONE && token != ERR) {
+        tokens[token]++;
+        if (token == ID) identifiers[lexeme]++;
+    }
 
+    if (token == ERR) return 1;
+
+    for (map<Token, int>::iterator it = tokens.begin(); it != tokens.end(); it++) {
+        tokensByCount.insert(pair<int, Token>(it->second, it->first));
+    }
+
+    int lastValue = -1;
+    int uniqueValues = 0;
+    for (multimap<int, Token>::reverse_iterator it = tokensByCount.rbegin(); it != tokensByCount.rend(); it++) {
+        if (lastValue != it->first && lastValue != -1)
+            uniqueValues++;
+
+        if (uniqueValues < 3) {
+            if (it->second == ID)
+                cout << "Identifier: " << it->first << endl;
+            if (it->second == STRING)
+                cout << "String constant: " << it->first << endl;
+            if (it->second == UNION)
+                cout << "Union operator: " << it->first << endl;
+            if (it->second == INTERSECT)
+                cout << "Intersect operator: " << it->first << endl;
+            if (it->second == SET)
+                cout << "SET keyword: " << it->first << endl;
+            if (it->second == PRINT)
+                cout << "PRINT keyword: " << it->first << endl;
+            if (it->second == SEARCH)
+                cout << "SEARCH keyword: " << it->first << endl;
+            if (it->second == FOR)
+                cout << "FOR keyword: " << it->first << endl;
+            if (it->second == LPAREN)
+                cout << "Left paren: " << it->first << endl;
+            if (it->second == RPAREN)
+                cout << "Right paren: " << it->first << endl;
+            if (it->second == SC)
+                cout << "Semicolon: " << it->first << endl;
+        }
+        lastValue = it->first;
+    }
+    if (identifiers.size() > 0)
+        cout << "List of identifiers: ";
+
+    for (map<string, int>::iterator it = identifiers.begin(); it != identifiers.end();) {
+        cout << it->first;
+        it++;
+        if (it != identifiers.end())
+            cout << ", ";
+    }
     return 0;
 }
